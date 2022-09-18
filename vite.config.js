58e21fc1655d730727dca6741eedfbe02e989509
zrfvnzr <106287050+zrfvnzr@users.noreply.github.com>
2022-09-18 22:48:01 +0800

    base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cmsc128.1-labexer1/', // https://dev.to/shashannkbawa/deploying-vite-app-to-github-pages-3ane
   plugins: [vue()]
 })
